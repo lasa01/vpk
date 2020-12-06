0b3983484775b27107b0ea33db7a23dcd2954cf3
Lassi Säike <lassisaike11@gmail.com>
2020-12-06 17:13:28 +0200

    Make package installable from git

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 from setuptools import setup
 from codecs import open
 from os import path
-import vpk
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -11,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='vpk',
-    version=vpk.__version__,
+    version='1.3.2',
     description='Library for working with Valve Pak files',
     long_description=long_description,
     url='https://github.com/ValvePython/vpk',
